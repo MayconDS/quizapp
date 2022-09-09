9737d7dd1803e6b027da09fe2aaa510f636a3bfd
Maycon Douglas <crossfireelite601@gmail.com>
2022-09-09 16:57:02 -0300

    questions refatored

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,15 +1,5 @@
 const questions = [
   {
-    question: 'Oque é Vanilla JavaScript',
-    options: [
-      'Um framework',
-      'Javascript puro',
-      'Uma biblioteca',
-      'Metodologia',
-    ],
-    answer: 'Javascript puro',
-  },
-  {
     question: 'Oque é ReactJS',
     options: [
       'Um framework',
@@ -19,16 +9,7 @@ const questions = [
     ],
     answer: 'Um framework',
   },
-  {
-    question: 'Qual desses não percorre um array',
-    options: ['map', 'forEach', 'for', 'length'],
-    answer: 'length',
-  },
-  {
-    question: 'Qual desses mostra o tamanho do item',
-    options: ['length', 'index', 'qty', 'sort'],
-    answer: 'length',
-  },
+
   {
     question: 'Qual desses é um operador bool',
     options: ['true e false', 'index', 'sort', 'math'],
@@ -40,77 +21,75 @@ const questions = [
     answer: 'Maycon',
   },
   {
-    question: "O que é Vanilla JavaScript?",
+    question: 'O que é Vanilla JavaScript?',
     options: [
-      "JavaScript puro",
-      "Uma biblioteca JavaScript",
-      "Um framework JavaScript",
-      "Um compilador de JavaScript",
+      'JavaScript puro',
+      'Uma biblioteca JavaScript',
+      'Um framework JavaScript',
+      'Um compilador de JavaScript',
     ],
-    answer: "JavaScript puro",
+    answer: 'JavaScript puro',
   },
   {
-    question: "Com qual instrução declaramos uma constante em JavaScript?",
-    options: ["const", "let", "var", "define"],
-    answer: "const",
+    question: 'Com qual instrução declaramos uma constante em JavaScript?',
+    options: ['const', 'let', 'var', 'define'],
+    answer: 'const',
   },
   {
-    question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
-    options: ["string", "number", "boolean", "float"],
-    answer: "float",
+    question: 'Qual dos tipos de dado a seguir não existe em JavaScript?',
+    options: ['string', 'number', 'boolean', 'float'],
+    answer: 'float',
   },
   {
-    question: "Qual dos métodos a seguir seleciona um elemento?",
-    options: ["querySelector", "parseInt", "sort", "reduce"],
-    answer: "querySelector",
+    question: 'Qual dos métodos a seguir seleciona um elemento?',
+    options: ['querySelector', 'parseInt', 'sort', 'reduce'],
+    answer: 'querySelector',
   },
   {
     question:
-      "Qual destas propriedades da a quantidade de elementos de um array?",
-    options: ["qty", "length", "items", "index"],
-    answer: "length",
+      'Qual destas propriedades da a quantidade de elementos de um array?',
+    options: ['qty', 'length', 'items', 'index'],
+    answer: 'length',
   },
   {
-    question: "Qual regra altera a cor de um elemento?",
-    options: ["color", "background-color", "font-size", "transition"],
-    answer: "color",
+    question: 'Qual regra altera a cor de um elemento?',
+    options: ['color', 'background-color', 'font-size', 'transition'],
+    answer: 'color',
   },
   {
-    question: "Para aumentar a fonte de um elemento utilizamos:",
-    options: ["font", "text-transform", "font-size", "hover"],
-    answer: "font-size",
+    question: 'Para aumentar a fonte de um elemento utilizamos:',
+    options: ['font', 'text-transform', 'font-size', 'hover'],
+    answer: 'font-size',
   },
   {
-    question: "A posição que deixa um elemento fixo é a:",
-    options: ["static", "absolute", "fixed", "relative"],
-    answer: "fixed",
+    question: 'A posição que deixa um elemento fixo é a:',
+    options: ['static', 'absolute', 'fixed', 'relative'],
+    answer: 'fixed',
   },
   {
-    question: "Qual tag cria um parágrafo?",
-    options: ["<p>", "<h1>", "<text>", "<ul>"],
-    answer: "<p>",
+    question: 'Qual tag cria um parágrafo?',
+    options: ['<p>', '<h1>', '<text>', '<ul>'],
+    answer: '<p>',
   },
   {
-    question: "Qual atributo adiciona um link para a tag a?",
-    options: ["alt", "href", "src", "link"],
-    answer: "href",
+    question: 'Qual atributo adiciona um link para a tag a?',
+    options: ['alt', 'href', 'src', 'link'],
+    answer: 'href',
   },
   {
-    question: "As listas não ordenadas tem a tag de:",
-    options: ["<ol>", "<ul>", "<li>", "<list>"],
-    answer: "<ul>",
+    question: 'As listas não ordenadas tem a tag de:',
+    options: ['<ol>', '<ul>', '<li>', '<list>'],
+    answer: '<ul>',
   },
   {
-    question: "Qual atributo deixa o input obrigatório?",
-    options: ["placeholder", "value", "required", "maxlength"],
-    answer: "required",
+    question: 'Qual atributo deixa o input obrigatório?',
+    options: ['placeholder', 'value', 'required', 'maxlength'],
+    answer: 'required',
   },
   {
-    question: "A tag semântica indicada para rodapés é a:",
-    options: ["div", "main", "section", "footer"],
-    answer: "footer",
+    question: 'A tag semântica indicada para rodapés é a:',
+    options: ['div', 'main', 'section', 'footer'],
+    answer: 'footer',
   },
-
-
 ]
 export default questions
